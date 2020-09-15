53f3f7343323c47c262884d534e51b775d82c98c
irwanfjr <44388676+irwanfjr@users.noreply.github.com>
2020-09-15 21:01:58 +0700

    berhasil menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -3,17 +3,30 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req, res) {
+exports.index = function (req, res) {
     response.ok("Aplikasi REST API telah berjalan.", res);
 };
 
 // MENAMPILKAN SEMUA DATA MAHASISWA
-exports.tampilSemuaMahasiswa = function(req, res) {
-    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
-        if(error) {
-            connection.log(error);
+exports.tampilSemuaMahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+        if (error) {
+            console.log(error);
         } else {
             response.ok(rows, res);
         }
     });
 };
+
+// MENAMPILKAN SEMUA DATA MAHASISWA BERDASARKAN ID NYA
+exports.tampilBerdasarkanId = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
+}
